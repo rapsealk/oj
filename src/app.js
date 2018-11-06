437af226d746c8ad8732b167c5413fab0cdc27b0
rapsealk <piono623@naver.com>
2018-11-06 13:26:49 +0900

    subprocess

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
-const submit = require('./routes/submit');
+//const submit = require('./routes/submit');
 
 const app = express();
 
@@ -25,7 +25,7 @@ app.use('/monaco', express.static(path.join(__dirname, 'node_modules/monaco-edit
 app.use('/axios', express.static(path.join(__dirname, 'node_modules/axios')));
 
 app.use('/', index);
-app.use('/submit', submit);
+//app.use('/submit', submit);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/utils/io.handler.js
+++ src/utils/io.handler.js
@@ -1,15 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const { spawn, execFileSync } = require('child_process');
+
 module.exports = io => {
 
-    io.on('connection', socket => {
-        console.log('socket connected!');
+	io.on('connection', socket => {
+		console.log('socket connected!');
 
-        socket.on('data', message => {
-            console.log('data:', message);
-            socket.emit('result', 'hello world!');
-        });
+		socket.on('data', code => {
+			console.log('data:', code);
 
-        socket.on('disconnect', () => {
-            console.log('socket disconnected!');
-        });
-    });
+			// check if dir "submits" exists.
+			try {
+				fs.statSync('submits');
+			} catch (e) {
+				if (e.errno === -4058) fs.mkdirSync('submits');
+			}
+
+			// Promise
+			const timestamp = Date.now();
+			const filename = `submits/${timestamp}.c`;
+
+			fs.writeFileSync(filename, code, 'utf-8');
+
+			// compile
+			const compile = spawn('gcc', [filename, '-o', `submits/${timestamp}`]);
+			compile.stdout.on('data', data => console.log('[compile:stdout]:', data.toString('utf-8')));
+			compile.stderr.on('data', data => console.log('[compile:stderr]:', data.toString('utf-8')));
+			compile.on('close', code => {
+				console.log('[compile:close]:', code);
+
+				// execute
+				const execute = spawn(`${path.join(__dirname, '../submits/' + timestamp)}`);
+				execute.stdout.on('data', data => console.log('[execute:stdout]:', data.toString('utf-8')));
+				execute.stderr.on('data', data => console.log('[execute:stderr]:', data.toString('utf-8')));
+				execute.on('error', message => console.log('[execute:error]:', message));
+				execute.on('close', code => console.log('[execute:close]:', code));
+			});
+
+			socket.emit('result', 'hello world!');
+		});
+
+		socket.on('disconnect', () => {
+			console.log('socket disconnected!');
+		});
+	});
 };

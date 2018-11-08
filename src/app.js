9323110110100cfa014b0fad68f68454df037d77
rapsealk <piono623@naver.com>
2018-11-08 20:59:40 +0900

    UI updates

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/monaco', express.static(path.join(__dirname, 'node_modules/monaco-editor')));
-app.use('/axios', express.static(path.join(__dirname, 'node_modules/axios')));
+app.use('/swal', express.static(path.join(__dirname, 'node_modules/sweetalert2')));
 
 app.use('/', index);
 //app.use('/submit', submit);

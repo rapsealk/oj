56c7f214bc0a723b5f007c5df4e8b7cdb3d19c20
rapsealk <piono623@naver.com>
2018-11-08 21:19:05 +0900

    stdout as file

--- src/test/output.js
+++ src/test/output.js
@@ -1,10 +1,9 @@
 const fs = require('fs');
 const { spawn } = require('child_process');
 
-const data = fs.readFileSync('data.txt', { encoding: 'utf-8' }).split('\n');
+const data = fs.readFileSync('./data.txt', { encoding: 'utf-8'}).split('\n');
 data.pop();
-
-console.log('data:', data);
+const outputs = [];
 
 const compile = spawn('gcc', ['a.c']);
 compile.stdout.on('data', data => console.log('[compile:stdout]:', data.toString('utf-8')));
@@ -13,9 +12,17 @@ compile.on('close', code => {
 	console.log('[compile:close]:', code);
 
 	const execute = spawn('./a.out');
-	execute.stdout.on('data', data => console.log('[execute:stdout]:', data.toString('utf-8')));
+	execute.stdout.on('data', data => {
+		data = data.toString('utf-8');
+		console.log('[execute:stdout]:', data);
+		outputs.push(data);
+	});
 	execute.stderr.on('data', data => console.log('[execute:stderr]:', data.toString('utf-8')));
-	execute.on('close', code => console.log('[execute:close]:', code));
+	execute.on('close', code => {
+		console.log('[execute:close]:', code);
+
+		fs.writeFileSync(`./${Date.now()}.txt`, outputs.join('\n'));
+	});
 	
 	execute.stdin.setEncoding('utf-8');
 

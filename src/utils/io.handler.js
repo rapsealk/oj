32cce14df1365cc9d48104e55b43b0b26579298b
rapsealk <piono623@naver.com>
2018-11-13 16:28:26 +0900

    input

--- src/utils/io.handler.js
+++ src/utils/io.handler.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { spawn } = require('child_process');
 
 const execution = {
-	"10": function(dirname, timestamp) {
+	"10": function(dirname, timestamp, socket) {
 		const inputs = fs.readFileSync(`${dirname}/20181109_in.txt`, { encoding: 'utf-8' }).split('\n');
 		inputs.pop();
 		const answers = fs.readFileSync(`${dirname}/20181109_out.txt`, { encoding: 'utf-8' }).split('\n');
@@ -25,25 +25,27 @@ const execution = {
 			execute.on('close', code => console.log('[execute:close]:', code));
 		});
 	},
-	"11": function(dirname, timestamp) {
-		const execute = spawn(`${dirname}/${timestamp}`);
-		execute.stdout.on('data', data => {
-			data = data.toString('utf-8').replace('\n', '');
-			console.log('[execute:stdout]:', data);
-			const expected = answers.shift();
-			const topic = (data == expected) ? 'correct' : 'exerror';
-			socket.emit(topic, `Expected: ${expected}, Output: ${data}`);
-		});
-		execute.stderr.on('data', data => console.log('[execute:stderr]:', data.toString('utf-8')));
-		execute.on('close', code => console.log('[execute:close]:', code));
-
-		execute.stdin.setEncoding('utf-8');
+	"11": function(dirname, timestamp, socket) {
 		
 		const inputs = fs.readFileSync(`${dirname}/20181116_in.txt`, { encoding: 'utf-8' }).split('\n');
 		const answers = fs.readFileSync(`${dirname}/20181116_out.txt`, { encoding: 'utf-8' }).split('\n');
 
-		inputs.forEach(input => execute.stdin.write(input+'\n'));
-		execute.stdin.end();
+		inputs.forEach((input, index) => {
+			const execute = spawn(`${dirname}/${timestamp}`);
+			execute.stdout.on('data', data => {
+				data = data.toString('utf-8').replace('\n', '');
+				console.log('[execute:stdout]:', data);
+				const topic = (data == answers[index]) ? 'correct' : 'exerror';
+				socket.emit(topic, `Expected: ${answers[index]}, Output: ${data}`);
+			});
+			execute.stderr.on('data', data => console.log('[execute:stderr]:', data.toString('utf-8')));
+			execute.on('error', message => console.log('[execute:error]:', message));
+			execute.on('close', code => console.log('[execute:close]:', code));
+	
+			execute.stdin.setEncoding('utf-8');
+			execute.stdin.write(input+'\n');
+			execute.stdin.end();
+		});
 	}
 };
 
@@ -74,7 +76,7 @@ module.exports = io => {
 
 			// compile
 			try {
-				const compile = spawn('gcc', [`${dirname}/${filename}`, '-o', `${dirname}/${timestamp}`, '-std=c99', '-Wall', '-static']);	// -O2
+				const compile = spawn('gcc', [`${dirname}/${filename}`, '-o', `${dirname}/${timestamp}`, '-std=c99', '-static']);	// -O2, -Wall
 				compile.stdout.on('data', data => console.log('[compile:stdout]:', data.toString('utf-8')));
 				compile.stderr.on('data', data => {
 					isCompileSuccessful = false;
@@ -90,29 +92,7 @@ module.exports = io => {
 					if (!isCompileSuccessful) return;
 
 					// execute
-					execution[week](dirname, timestamp);
-					/*
-					const inputs = fs.readFileSync(`${dirname}/20181109_in.txt`, { encoding: 'utf-8' }).split('\n');
-					inputs.pop();
-					const answers = fs.readFileSync(`${dirname}/20181109_out.txt`, { encoding: 'utf-8' }).split('\n');
-					inputs.forEach((input, index) => {
-						const execute = spawn(`${dirname}/${timestamp}`, input.split(' '));
-						execute.stdout.on('data', data => {
-							data = data.toString('utf-8').replace('\n', '');
-							const equals = (data == answers[index]);
-							console.log('[execute:stdout]:', data, '[expected]:', answers[index], '[equals]:', equals);
-							if (!equals) {
-								socket.emit('exerror', `Input: ${input}, Expected: ${answers[index]}, Output: ${data}`);
-								execute.kill('SIGKILL');
-							} else {
-								socket.emit('correct', `Input: ${input}, Expected: ${answers[index]}, Output: ${data}`);
-							}
-						});
-						execute.stderr.on('data', data => console.log('[execute:stderr]:', data.toString('utf-8')));
-						execute.on('error', message => console.log('[execute:error]:', message));
-						execute.on('close', code => console.log('[execute:close]:', code));
-					});
-					*/
+					execution[week](dirname, timestamp, socket);
 				});
 			} catch (e) {
 				console.log('[compile_error]:', e);
